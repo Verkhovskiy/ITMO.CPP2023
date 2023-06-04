4df76f60acbdeb71a6e830ee91b531e6a538bf84
Verkhovskiy <artemverhovskiy@itcenterifmo.onmicrosoft.com>
2023-06-04 14:31:40 +0300

    Lab10 update

--- Lab10/FinTask/Aggregation/triangle.cpp
+++ Lab10/FinTask/Aggregation/triangle.cpp
@@ -5,16 +5,16 @@
 class Triangle
 {
     private:
-        Dot dots[3];
+        Dot* dots[3];
         double sideLengths[3];
     public:
-        Triangle(Dot &first, Dot &second, Dot &third) {
+        Triangle(Dot *first, Dot *second, Dot *third) {
             dots[0] = first;
             dots[1] = second;
             dots[2] = third;
-            sideLengths[0] = dots[0].distanceTo(dots[1]);
-            sideLengths[1] = dots[1].distanceTo(dots[2]);
-            sideLengths[2] = dots[2].distanceTo(dots[0]);
+            sideLengths[0] = dots[0]->distanceTo(*dots[1]);
+            sideLengths[1] = dots[1]->distanceTo(*dots[2]);
+            sideLengths[2] = dots[2]->distanceTo(*dots[0]);
         }
         void printSides() {
             std::cout << "The lengths of triangle sides are: " << std::endl << sideLengths[0] << std::endl << sideLengths[1] << std::endl << sideLengths[2];
